6e6f9db894d123f0fbd1124dc7a7095adf337bf7
sskim91 <tjdtjq91@gmail.com>
2021-03-27 13:09:58 +0900

    Feat: 스프링 MVC 기본기능 - 요청 매핑 API 예시

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,40 @@
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by sskim on 2021/03/27
+ * Github : http://github.com/sskim91
+ */
+@Slf4j
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+
+    @GetMapping
+    public String user() {
+        return "get users";
+    }
+
+    @PostMapping
+    public String addUser() {
+        return "post user";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userId) {
+        return "get userId=" + userId;
+    }
+
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable String userId) {
+        return "update userId=" + userId;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        return "delete userId=" + userId;
+    }
+}
