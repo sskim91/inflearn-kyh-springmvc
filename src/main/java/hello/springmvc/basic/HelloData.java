26e8e9669efc5eb0582dbdc141bd2138652c3722
sskim91 <tjdtjq91@gmail.com>
2021-03-27 15:25:12 +0900

    Feat: 스프링 MVC 기본기능 - HTTP 요청 파라미터 쿼리 파라미터, HTML Form @RequestParam @ModelAttribute

---
+++ src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,14 @@
+package hello.springmvc.basic;
+
+import lombok.Data;
+
+/**
+ * Created by sskim on 2021/03/27
+ * Github : http://github.com/sskim91
+ */
+@Data
+public class HelloData {
+
+    private String username;
+    private int age;
+}
